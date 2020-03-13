e4e51c3b231e2d22bb27eb5ecf9e900dd757b231
cryingpotato <qq1591680@gmail.com>
2020-03-13 20:38:45 +0800

    加入專案檔案。

---
+++ QWER/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace QWER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello World!");
+            int a = 100;
+            while (true) 
+            {
+                if (a<=0)
+                {
+                    Console.WriteLine("QQ");
+                    break;
+                }
+                string input = Console.ReadLine();
+                int attack = int.Parse(input);
+                a = a - attack;
+
+                           
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
